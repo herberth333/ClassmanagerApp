c6540c8ca7ae283281a6d807426c685cdd06d138
kakaoliveiiira <kakatrindade16@hotmail.com>
2026-05-21 21:59:53 -0300

    Atualizar navegação da tela inicial para incluir Acadêmico

--- classmanagerapp/lib/screens/home/home_sreen.dart
+++ classmanagerapp/lib/screens/home/home_sreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:classmanagerapp/core/widgets/navbar/navbar.dart';
-import 'package:classmanagerapp/core/widgets/diciplinas/diciplinas.dart'; 
+import 'package:classmanagerapp/core/widgets/diciplinas/diciplinas.dart';
+import 'package:classmanagerapp/screens/home/academico_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,24 +11,24 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _abaAtiva = 0; 
+  int _abaAtiva = 0;
 
   final List<Widget> _telas = [
-    const DiciplinasScreen(), 
-    const Scaffold(body: Center(child: Text('Conteúdo do Acadêmico', style: TextStyle(fontSize: 20)))),
+    const DiciplinasScreen(),
+    const AcademicoScreen(),
     const Scaffold(body: Center(child: Text('Conteúdo das Configurações', style: TextStyle(fontSize: 20)))),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _telas[_abaAtiva], 
+      body: _telas[_abaAtiva],
 
       bottomNavigationBar: CustomNavbar(
         currentIndex: _abaAtiva,
         onTap: (index) {
           setState(() {
-            _abaAtiva = index; 
+            _abaAtiva = index;
           });
         },
       ),
